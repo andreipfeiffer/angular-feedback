e725d239fa27bae098e5ac9a4d11a87fc5700ce7
Andrei Pfeiffer <andrei@upsidedown.ro>
2014-11-28 16:33:18 +0200

    Hide whole markup when not displayed

--- src/scripts/feedback.js
+++ src/scripts/feedback.js
@@ -36,8 +36,11 @@
                 var timeoutDismiss;
 
                 var feedbackScope = $rootScope.$new();
+
+                feedbackScope.isActive = false;
+
                 var tpl = $compile(
-                    '<div class="fdb">' +
+                    '<div class="fdb" ng-if="isActive">' +
                         '<div class="fdb-inner" ng-class="feedbackClass" ng-click="!isLoading && dismiss()">' +
                             '<span class="fdb-message" ng-if="!isLoading">{{ message }}</span>' +
                             '<span class="fdb-spinner" ng-if="isLoading"></span>' +
@@ -63,6 +66,7 @@
                 };
 
                 var reset = function() {
+                    feedbackScope.isActive = false;
                     feedbackScope.feedbackClass = '';
                     feedbackScope.message = '';
                     feedbackScope.isLoading = false;
@@ -119,6 +123,7 @@
                         c += sticky ? ' fdb-sticky' : '';
                         c += ' fdb-expand';
 
+                        feedbackScope.isActive = true;
                         feedbackScope.feedbackClass = c;
                         feedbackScope.isLoading = false;
                         feedbackScope.message = message;
@@ -136,6 +141,7 @@
 
                         var c = setType('neutral') + ' ' + ' fdb-loading';
 
+                        feedbackScope.isActive = true;
                         feedbackScope.feedbackClass = c;
                         feedbackScope.message = '';
                         feedbackScope.isLoading = true;
