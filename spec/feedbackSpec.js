86acb18a8fcccd133457f6624b1f7943c1d0f47c
Andrei Pfeiffer <andrei@upsidedown.ro>
2014-11-29 17:39:16 +0200

    Bootstrap tests

---
+++ spec/feedbackSpec.js
@@ -0,0 +1,31 @@
+(function() {
+
+    'use strict';
+
+    describe('angular-feedback', function() {
+
+        var feedbackProvider;
+
+        beforeEach(function() {
+            // Here we create a fake module just to intercept and store the provider
+            // when it's injected, i.e. during the config phase.
+            angular
+                .module('fakeModule', function() {})
+                .config(['feedbackProvider', function(provider) {
+                    feedbackProvider = provider;
+                }]);
+
+            module('angular-feedback', 'fakeModule');
+
+            // This actually triggers the injection into fakeModule
+            inject(function(){});
+        });
+
+        it('should be inactive by default', inject(function(feedback) {
+            expect( feedback.isActive() ).toBeFalsy();
+            expect( feedback.isLoading() ).toBeFalsy();
+        }));
+
+    });
+
+})();
